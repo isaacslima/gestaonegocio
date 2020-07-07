f698dd439f80e06b937c379e74f37b4bcea96a4f
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-07-06 23:05:21 -0300

    show like cards clients and services

--- src/views/CadastroClientes.vue
+++ src/views/CadastroClientes.vue
@@ -2,87 +2,32 @@
   <div class="main-content-container container-fluid px-4">
     <!-- Page Header -->
     <div class="page-header row no-gutters py-4">
-      <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
+      <div class="col-12 col-sm-9 text-center text-sm-left mb-0">
         <h3 class="page-title">Clientes</h3>
       </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <v-card>
-          <v-card-title>
-            <v-text-field
-              v-model="search"
-              append-icon="mdi-magnify"
-              label="Search"
-              single-line
-              hide-details
-            ></v-text-field>
-          </v-card-title>
-          <v-data-table
-            :headers="headers"
-            :items="clientes"
-            :search="search"
-          >
-            <template slot="items" slot-scope="props" >
-              <td  @click="mostraCliente(props.item['.key'])">{{ props.item.nome }}</td>
-              <td >{{ props.item.telefone }}</td>
-              <td >{{ props.item.email }}</td>
-              <td >{{ props.item.cidade }}</td>
-              <td >{{ props.item.dataContato }}</td>
-              <td >{{ props.item.dataParto }}</td>
-              <td align="center">
-                <v-icon color="green" v-if="props.item.interessado">check_circle_outline</v-icon>
-              </td>
-              <td align="center">
-                <v-icon color="green" v-if="props.item.formulario">check_circle_outline</v-icon>
-              </td>
-              <td >{{ props.item.followup }}</td>
-              <td>
-                <v-btn-toggle v-model="toggle_multiple" multiple>
-                  <v-btn flat color="blue" @click="editaCliente(props.item['.key'])">
-                    <v-icon>edit</v-icon>
-                  </v-btn>
-                  <v-btn flat color="red" @click="desativar(props.item['.key'])" >
-                    <v-icon>close</v-icon>
-                  </v-btn>
-                </v-btn-toggle>
-              </td>
-            </template>
-          </v-data-table>
-        </v-card>
-        <v-dialog
-      v-model="dialog"
-      max-width="290"
-    >
-      <v-card>
-        <v-card-title class="headline">Confirmar Exclusão</v-card-title>
-
-        <v-card-text>
-          Deseja realmente excluir o(a) Cliente?
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-
-          <v-btn
-            color="red darken-1"
-            flat="flat"
-            @click="dialog = false, keyExclusao = null"
-          >
-            Cancelar
-          </v-btn>
-
-          <v-btn
-            color="green darken-1"
-            flat="flat"
-            @click="remover()"
-          >
-            Excluir
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+      <div class="col-3 col-sm-3 text-center text-sm-right mb-0">
+        <d-button theme="success" class="mb-2 mr-1">Novo Cliente</d-button>
       </div>
+    </div>
+
+    <div class="col-12 col-sm-12 text-center text-sm-left mb-0">
+      <d-card class="card-small mb-4" v-for="item in clientes" :key="item['.key']">
+        <d-list-group flush>
+          <d-list-group-item class="px-3">
+            <h5>
+              <b>Nome:</b> {{ item.nome }} <b>Telefone:</b> {{ item.telefone }}
+              <b>Interessado: </b>
+                <i class="material-icons mr-1 bg-success rounded text-white" v-if="item.interessado" >check</i>
+                <i class="material-icons mr-1 bg-danger rounded text-white" v-if="!item.interessado">close</i>
+            </h5>
+          </d-list-group-item>
+        </d-list-group>
+        <d-list-group flush class="text-right">
+          <d-button size="sm" theme="primary" class="mb-2 btn-outline-light mr-1">
+            <i class="material-icons mr-1 bg-primary text-white">edit</i>Editar
+          </d-button>
+        </d-list-group>
+      </d-card>
     </div>
   </div>
 </template>
