9139aaf3bb49432f2df5893f81924323179ff411
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-08-17 00:09:51 -0300

    changing layout invoices

--- src/views/Entradas.vue
+++ src/views/Entradas.vue
@@ -1,37 +1,31 @@
 <template>
-  <div class="main-content-container container-fluid px-4">
-    <!-- Page Header -->
-    <div class="page-header row no-gutters py-4">
-      <div class="col-12 col-sm-9 text-center text-sm-left mb-0">
-        <h3 class="page-title">Entradas</h3>
-      </div>
-      <div class="col-3 col-sm-3 text-center text-sm-right mb-0">
-        <v-btn small style="background-color: green" dark class="mb-2 mr-1" @click="editInvoice('new')">Nova Entrada</v-btn>
-      </div>
-    </div>
-    <div class="col-12 col-sm-12 text-center text-sm-left mb-0">
-      <d-card class="card-small mb-4" v-for="item in entradas" :key="item['.key']">
-        <d-list-group flush>
-          <d-list-group-item class="px-3">
-            <h5>
-              <b>Cliente: </b> {{ item.cliente }} <b>Data: </b>{{ item.data }} <b>Preço: </b> R$ {{ item.preco.toFixed(2) }} <b>Serviços: </b> {{ item.servico }}
-            </h5>
-            <div class="row">
-              <d-card class="col-3 m-1" v-for="e in item.entradas" :key="e['.key']" >
-                <p><b>Pago:</b>
-                  <i class="material-icons mr-1 bg-success rounded text-white" v-if="e.pago" >check</i>
-                <i class="material-icons mr-1 bg-danger rounded text-white" v-if="!e.pago">close</i>
-                   <b>Valor:</b> {{ e.valor.toFixed(2) }} <br><b>Data Pagamento:</b> {{ e.dataPagamento }}</p>
-              </d-card>
-            </div>
-            <div class="text-right">
-              <v-btn small style="background-color: blue" dark class="mb-2 btn-outline-light mr-1" @click="editInvoice(item.key)">
-                <i class="material-icons mr-1 text-white">edit</i>Editar
-              </v-btn>
-            </div>
-          </d-list-group-item>
-        </d-list-group>
-      </d-card>
+  <div>
+    <v-app-bar dense app style="background-color: #759F89" color="blue-grey" dark >
+      Cadastro de Entradas
+      <v-spacer></v-spacer>
+      <v-btn small style="background-color: green" dark class="mb-2 mr-1" @click="editInvoice('new')">Nova Entrada</v-btn>
+    </v-app-bar>
+    <div class="col-12 col-sm-12 text-center text-sm-left mb-0 mt-4">
+      <v-card class="card-small mb-4" v-for="item in entradas" :key="item['.key']">
+        <v-card-text class="px-3">
+          <h5>
+            <b>Cliente: </b> {{ item.cliente }} <b>Data: </b>{{ item.data }} <b>Preço: </b> R$ {{ item.preco.toFixed(2) }} <b>Serviços: </b> {{ item.servico }}
+          </h5>
+          <div class="row">
+            <v-card class="col-3 m-1" v-for="e in item.entradas" :key="e['.key']" >
+              <p><b>Pago:</b>
+                <i class="material-icons mr-1 bg-success rounded text-white" v-if="e.pago" >check</i>
+              <i class="material-icons mr-1 bg-danger rounded text-white" v-if="!e.pago">close</i>
+                  <b>Valor:</b> {{ e.valor.toFixed(2) }} <br><b>Data Pagamento:</b> {{ e.dataPagamento }}</p>
+            </v-card>
+          </div>
+          <div class="text-right">
+            <v-btn small style="background-color: blue" dark class="mb-2 btn-outline-light mr-1" @click="editInvoice(item.key)">
+              <i class="material-icons mr-1 text-white">edit</i>Editar
+            </v-btn>
+          </div>
+        </v-card-text>
+      </v-card>
     </div>
   </div>
 </template>
