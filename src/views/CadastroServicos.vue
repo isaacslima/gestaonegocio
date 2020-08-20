89ea0fd4b5c138829625fd3eabfe07e34ccfa20a
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-08-20 17:31:29 -0300

    add function to remove service

--- src/views/CadastroServicos.vue
+++ src/views/CadastroServicos.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <v-app id="service">
     <v-app-bar dense app style="background-color: #759F89" color="blue-grey" dark >
       Cadastro de Serviços
       <v-spacer></v-spacer>
@@ -18,14 +18,43 @@
               <v-btn small style="background-color: blue" dark class="mb-2 btn-outline-light mr-1" @click="editService(item.key)">
                 <i class="material-icons mr-1 text-white">edit</i>Editar
               </v-btn>
-              <v-btn small style="background-color: red" dark class="mb-2 btn-outline-light mr-1" @click="confirmRemoveService(item.key)">
+              <v-btn small style="background-color: red" dark class="mb-2 btn-outline-light mr-1" @click="confirmRemoveService(item.key, item.nome)">
                 <i class="material-icons mr-1 text-white">close</i>Remover
               </v-btn>
             </div>
           </v-card-text>
       </v-card>
     </div>
-  </div>
+    <v-row>
+      <v-dialog v-model="dialog" max-width="360">
+        <v-card>
+          <v-card-title class="headline">Deseja realmente remover o serviço?</v-card-title>
+          <v-card-text>
+            {{ nome }}
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn
+              color="green darken-1"
+              text
+              @click="dialog = false"
+            >
+              Não
+            </v-btn>
+
+            <v-btn
+              color="green darken-1"
+              text
+              @click="removeService()"
+            >
+              Sim
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+  </v-app>
 </template>
 
 <script>
@@ -40,6 +69,7 @@ export default {
     search: '',
     servicos: [],
     dialog: false,
+    nome: '',
     keyExclusao: false,
     keys: [],
   }),
@@ -53,29 +83,34 @@ export default {
     verifyLogin() {
       this.$emit('logou');
     },
-    confirmRemoveService(key) {
+    confirmRemoveService(key, nome) {
       this.dialog = true;
+      this.nome = nome;
       this.keyExclusao = key;
     },
     removeService() {
       servicosRef.child(this.keyExclusao).remove();
+      this.updateListServices();
       this.dialog = false;
+    },
+    updateListServices() {
+      const self = this;
+      servicosRef.on('child_added', (snapshot) => {
+        const servico = {
+          key: snapshot.key,
+          nome: snapshot.val().nome,
+          preco: snapshot.val().preco,
+          observacao: snapshot.val().observacao,
+        };
+        self.servicos.push(servico);
+      });
     },
   },
   created() {
     this.verifyLogin();
   },
   beforeMount() {
-    const self = this;
-    servicosRef.on('child_added', (snapshot) => {
-      const servico = {
-        key: snapshot.key,
-        nome: snapshot.val().nome,
-        preco: snapshot.val().preco,
-        observacao: snapshot.val().observacao,
-      };
-      self.servicos.push(servico);
-    });
+    this.updateListServices();
   },
 };
 </script>
